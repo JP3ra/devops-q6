d8d6293b86d75879d0e796aee62a27f11c3a8ff2
Dr. Prajnya Sarangi <jyotiprakash2409@gmail.com>
2025-01-03 19:53:18 +0530

    change

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from jenkins!"
+    return "Hello from jenkins! this is a s"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
